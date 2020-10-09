afd9d0d9c03a9664c43654653ccb53f8cd040540
Sagar Budhathoki Magar <sbmagar.sbm99@gmail.com>
2020-10-09 20:15:30 +0545

    Update WeatherAPI_Lambda.py

--- WeatherAPI_Lambda.py
+++ WeatherAPI_Lambda.py
@@ -4,10 +4,13 @@ import time
 import os
 import logging
 
+# from datetime import datetime
+
+
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-OWM_API_KEY = "*************************"
+OWM_API_KEY = "d9c63d9f66edfc2c2b3a87d5abf9f64e"
 
 
 def build_response(message):
@@ -89,71 +92,127 @@ def try_ex(func):
 
 
 
-def get_weather(intent_request):
+def get_weather_forecast(intent_request):
+    
     try:
         location = intent_request['currentIntent']['slots']['city']
     except KeyError:
         location = intent_request['currentIntent']['slots']['City']
 
-    # source = intent_request['invocationSource']
-    # slots = intent_request['currentIntent']['slots']
+    source = intent_request['invocationSource']
+    slots = intent_request['currentIntent']['slots']
 
     session_attributes = intent_request['sessionAttributes'] if intent_request['sessionAttributes'] is not None else {}
 
 
-#     if source == 'DialogCodeHook':
-#         if slots['city'] is None:
-#             return elicit_slot(
-#                 intent_request['currentIntent']['name'],
-#                 slots,
-#                 'city',
-#                 'Please enter city name'
-#             )
-#         return delegate(session_attributes, slots)
-#
-#     if source == 'FulfillmentCodeHook':
-#         result = weather_api_call(OWM_API_KEY, slots['city'])
-#         return build_response(result)
-#
-#
-# def weather_api_call(OWM_API_KEY, location):
-#     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format(slots['city'], OWM_API_KEY)
-#     r = requests.get(url)
-#     return r.json()
+    if source == 'DialogCodeHook':
+        if slots['city'] is None:
+            return elicit_slot(
+                session_attributes,
+                intent_request['currentIntent']['name'],
+                slots,
+                'city',
+                {
+                    'contentType':'PlainText',
+                    'content':'Please enter city or country name to get weather informations about (or you can enter lattitude and longitude of location respectively)'
+                }
+            )
+        return delegate(session_attributes, slots)
+        
 
-
-    # complete_location = requests.get("api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + OWM_API_KEY)
-
-    response = ""
-
-    # Add Current Conditions to Response
-    current_weather = requests.get("http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(location, OWM_API_KEY)).json()
-
-    response += "Currently in %s, it is %.1f degrees fahrenheit, with winds %s. " % (
-        intent_request['currentIntent']['slots']['city'],
-        current_weather['main']['temp_min'],
-        current_weather['wind']['speed']
-    )
-
-    # Add Forecast to Response
-    # forecast = requests.get("http://api.wunderground.com/api/%s/forecast/%s.json" % (
-    #     OWM_API_KEY,
-    #     location_quick_link
-    # )).json()
-    #
-    # response += "The forecast for today from openweathermap has %s" % forecast['forecast']['txt_forecast']['forecastday'][0]['fcttext']
-
-
-    # Submit Response back to Lex
-
-    return close(
-        session_attributes,
-        'Fulfilled',
-        {
-            'contentType': 'PlainText',
-            'content': response
-        }
-    )
+    if source == 'FulfillmentCodeHook':
+        response = ""
+    
+        current_weather = requests.get("http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(location, OWM_API_KEY)).json()
+    
+        response += "Current weather condition in %s:  %s," % (intent_request['currentIntent']['slots']['city'],
+            current_weather['weather'][0]['main']) + "\n Temperature {:.2f} degree celsius, \n Winds {} mph, \n Pressure level {} millibars, \n Humidity {}%".format(
+            current_weather['main']['temp'] - 273.15,
+            current_weather['wind']['speed'],
+            current_weather['main']['pressure'],
+            current_weather['main']['humidity']
+        )
+        
+        return close(
+            session_attributes,
+            'Fulfilled',
+            {
+                'contentType': 'PlainText',
+                'content': response
+            }
+        )
+            
+            
+def get_weather_now(intent_request):
+    source = intent_request['invocationSource']
+    slots = intent_request['currentIntent']['slots']
+    
+    session_attributes = intent_request['sessionAttributes'] if intent_request['sessionAttributes'] is not None else {}
+    
+    if source == 'DialogCodeHook':
+        if slots['lat'] is None or slots['lon'] is None:
+            return elicit_slot(
+                session_attributes,
+                intent_request['currentIntent']['name'],
+                slots,
+                'city',
+                {
+                    'contentType':'PlainText',
+                    'content':'Please enter lattitude and longitude respectively'
+                }
+            )
+            
+        elif ('-90' > slots['lat'] > '90') or ('-180' > slots['lon'] > '180'):
+            return elicit_slot(
+                session_attributes,
+                intent_request['currentIntent']['name'],
+                slots,
+                'lon',
+                {
+                    'contentType':'PlainText',
+                    'content':'Lattitude or Longitude value invalid. lat(-90 to 90), lon(-180 to 180)'
+                }
+            )
+        
+        
+    if source == 'FulfillmentCodeHook':
+        lattitude = intent_request['currentIntent']['slots']['lat']
+        longitude = intent_request['currentIntent']['slots']['lon']
+        
+        response = ""
+    
+        current_weather = requests.get("http://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=hourly,alerts,minutely,current&appid={}".format(lattitude, longitude, OWM_API_KEY)).json()
+    
+        # current_date = ""
+    
+        # date_list = []
+        # for i in range(len(current_weather['daily'])):
+        #     date_list = datetime.fromtimestamp(current_weather['daily'][i]['dt'])
+            
+        
+        # for item in current_weather['daily']:
+        #     date = datetime.fromtimestamp(dt[item])
+            
+    
+        response += "Weather forecast for tomorrow in location coordinates %s and %s:--- " % (intent_request['currentIntent']['slots']['lat'],
+            intent_request['currentIntent']['slots']['lon'])  + " \n Temperature: {:.2f} degree celsius,\n Winds: {} mph,\n Pressure level: {} millibars, \n Humidity: {}% ".format(
+            # current_weather['daily'][0]['weather'][0]['main'],
+            current_weather['daily'][1]['temp']['day'] - 273.15,
+            current_weather['daily'][1]['wind_speed'],
+            current_weather['daily'][1]['pressure'],
+            current_weather['daily'][1]['humidity']
+        )
+            
+        
+        return close(
+            session_attributes,
+            'Fulfilled',
+            {
+                'contentType': 'PlainText',
+                'content': response
+            }
+        )    
+        
 
 
 def dispatch(intent_request):
@@ -163,8 +222,12 @@ def dispatch(intent_request):
 
     # Dispatch to your bot's intent handlers
     if intent_name == 'WeatherForecast':
-        return get_weather(intent_request)
-        raise Exception('Intent with name ' + intent_name + ' not supported')
+        return get_weather_forecast(intent_request)
+        
+    elif intent_name == 'WeatherNow':
+        return get_weather_now(intent_request)
+    
+    raise Exception('Intent with name ' + intent_name + ' not supported')
 
 
 
@@ -173,3 +236,4 @@ def lambda_handler(event, context):
     logger.debug('event.bot.name={}'.format(event['bot']['name']))
 
     return dispatch(event)
+
